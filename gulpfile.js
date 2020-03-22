e29f5350e8cc1162c043dfcf4c648e2a0f97c6fd
Munez3 <melwin03@gmail.com>
2020-03-22 11:47:51 +0100

    configure gulpfile to watch scss on remote

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('compass', function() {
         console.log(error.message);
         this.emit('end');
     }}))
-    .pipe(compass({ 
+    .pipe(compass({
       config_file: 'app/config.rb',
       css: 'app/stylesheets',
       sass: 'app/sass'
@@ -38,4 +38,28 @@ gulp.task('watch', ['browserSync', 'compass'], function(){
   gulp.watch('app/sass/**/*.scss', ['compass']);
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+
+gulp.task('compassWP', function() {
+  return gulp.src('remote/wp-content/themes/woreczki/sass/screen.scss')
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(compass({
+      config_file: 'app/config.rb',
+      css: 'remote/wp-content/themes/woreczki/stylesheets',
+      sass: 'remote/wp-content/themes/woreczki/sass'
+    }))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('remote/wp-content/themes/woreczki/stylesheets'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('watchWP', ['compassWP'], function(){
+  gulp.watch('remote/wp-content/themes/woreczki/sass/**/*.scss', ['compassWP']);
 });
